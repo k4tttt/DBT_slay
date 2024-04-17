5b40a21c7308cb676f991a8e22fe7079b290c5b8
id21ash <124149502+id21ash@users.noreply.github.com>
2024-04-17 16:12:41 +0200

    help

---
+++ main.js
@@ -0,0 +1,44 @@
+import './style.css'
+import Phaser from 'phaser'
+
+const sizes = {
+  width:500,
+  height:500
+};
+
+const speedDown = 300;
+
+class GameScene extends Phaser.Scene {
+  constructor() {
+    super("scene-game");
+  }
+
+  preload() {
+    this.load.image("bg", "C:\\Users\\alvas\\OneDrive\\Dokument\\DBT\\DBT_slay\\public\\assets\\bg.png");
+  }
+
+  create() {
+    this.add.image(0,0,"bg").setOrigin(0,0);
+  }
+
+  update() {
+  }
+
+}
+
+const config = {
+  type: Phaser.WEBGL,
+  width: sizes.width,
+  height: sizes.height,
+  canvas: gameCanvas,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: speedDown },
+      debug: true
+    }
+  },
+  scene: [GameScene]
+};
+
+const game = new Phaser.Game(config);
