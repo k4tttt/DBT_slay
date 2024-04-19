96f68c77694516ed6726fbbee78065364ff61582
k4tttt <tyra.woden@gmail.com>
2024-04-19 09:21:16 +0200

    working game
    no particles :(

--- testgame/main.js
+++ testgame/main.js
@@ -8,23 +8,126 @@ const sizes = {
 
 const speedDown = 300;
 
+const gameStartDiv = document.querySelector("#gameStart");
+const gameEndDiv = document.querySelector("#gameEnd");
+const gameStartBtn = document.querySelector("#startBtn");
+const gameWinLoseSpan = document.querySelector("#winLoseSpan");
+const gameEndScoreSpan = document.querySelector("#endScoreSpan");
+
 class GameScene extends Phaser.Scene {
   constructor() {
     super("scene-game");
     this.player;
+    this.cursor;
+    this.playerSpeed = speedDown + 100;
+    this.target;
+    this.points = 0;
+    this.textScore;
+    this.textTime;
+    this.timedEvent;
+    this.remainingTime;
+    this.woum;
+    this.bgMusic;
+    this.emitter;
   }
 
   preload() {
     this.load.image("bg", "/assets/bg.png");
     this.load.image("bar", "/assets/bar.png");
+    this.load.image("arrow", "/assets/arrow.png");
+    this.load.image("particle", "/assets/particle.png");
+    this.load.audio("bgMusic", "/assets/1-01 44th Cloud Lavender (SOPHIE Remix).mp3");
+    this.load.audio("woum", "/assets/hmm.mp3");
   }
 
   create() {
+    this.scene.pause("scene-game");
+
+    this.woum = this.sound.add("woum");
+    this.bgMusic = this.sound.add("bgMusic");
+    this.bgMusic.play();
+
     this.add.image(0, 0, "bg").setOrigin(0,  0);
-    this.player = this.add.image(0, sizes.height - 50, "bar").setOrigin(0,  0);
+    this.player = this.physics.add.image(0, sizes.height - 50, "bar").setOrigin(0,  0);
+    this.player.setImmovable(true);
+    this.player.body.allowGravity = false;
+    this.player.setCollideWorldBounds(true);
+    this.player.setSize(this.player.width - this.player.width/4, this.player.height/6).setOffset(this.player.width/10, this.player.height - this.player.height/10);
+
+    this.target = this.physics.add.image(0, 0, "arrow").setOrigin(0, 0);
+    this.target.setMaxVelocity(0, speedDown);
+
+    this.physics.add.overlap(this.target, this.player, this.targetHit, null, this);
+
+    this.cursor = this.input.keyboard.createCursorKeys();
+
+    this.textScore = this.add.text(sizes.width - 120, 10, "Score: 0", {
+      font: "25px Arial",
+      fill: "#000",
+    });
+    this.textTime = this.add.text(10, 10, "Remaining time: 00", {
+      font: "25px Arial",
+      fill: "#000",
+    });
+
+    this.timedEvent = this.time.delayedCall(80000, this.gameOver, [], this);
+
+    this.emitter = this.add.particles(0, 0, "particle", {
+      speed: 100,
+      gravityY: speedDown - 200,
+      scale: 0.04,
+      duration: 100,
+      emitting: false
+    });
+    this.emitter.startFollow(this.player, this.player.width/2, this.player.height/2, true);
   }
 
-  update() {}
+  update() {
+    this.remainingTime = this.timedEvent.getRemainingSeconds();
+    this.textTime.setText(`Remaining time: ${Math.round(this.remainingTime).toString()}`);
+
+    if (this.target.y >= sizes.height) {
+      this.target.setY(0);
+      this.target.setX(this.getRandomX());
+    }
+
+    const {left, right} = this.cursor;
+
+    if (left.isDown) {
+      this.player.setVelocityX(-this.playerSpeed);
+    } else if (right.isDown) {
+      this.player.setVelocityX(this.playerSpeed);
+    } else {
+      this.player.setVelocityX(0);
+    }
+  }
+
+  getRandomX() {
+    return Math.floor(Math.random() * 740);
+  }
+
+  targetHit() {
+    this.woum.play();
+    this.emitter.start();
+    this.target.setY(0);
+    this.target.setX(this.getRandomX());
+    this.points++;
+    this.textScore.setText(`Score: ${this.points}`);
+  }
+
+  gameOver() {
+    this.sys.game.destroy(true);
+
+    if (this.points >= 20) {
+      gameEndScoreSpan.textContent = this.points;
+      gameWinLoseSpan.textContent = "win!";
+    } else {
+      gameEndScoreSpan.textContent = this.points;
+      gameWinLoseSpan.textContent = "lose!";
+    }
+
+    gameEndDiv.style.display = "flex";
+  }
 }
 
 const config = {
@@ -43,3 +146,8 @@ const config = {
 };
 
 const game = new Phaser.Game(config);
+
+gameStartBtn.addEventListener("click", () => {
+  gameStartDiv.style.display = "none";
+  game.scene.resume("scene-game");
+});
