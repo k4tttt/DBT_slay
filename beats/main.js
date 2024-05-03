e3aff1a3cbdf5a181599f3e3488b95ba73f4a03b
k4tttt <tyra.woden@gmail.com>
2024-05-03 09:54:02 +0200

    testing arrow states

--- beats/main.js
+++ beats/main.js
@@ -6,13 +6,14 @@ const sizes = {
   height: 816
 };
 const beat = 1000;
+let time = 0;
 
 const gameStartDiv = document.querySelector("#gameStart");
 const gameEndDiv = document.querySelector("#gameEnd");
 const gameStartBtn = document.querySelector("#startBtn");
 const gameWinLoseSpan = document.querySelector("#winLoseSpan");
 const gameEndScoreSpan = document.querySelector("#endScoreSpan");
-
+ 
 class GameScene extends Phaser.Scene {
   constructor() {
     super("scene-game");
@@ -20,6 +21,7 @@ class GameScene extends Phaser.Scene {
     this.kick;
     this.quadrants;
     this.target;
+    this.sprite;
   }
 
   preload() {
@@ -29,15 +31,20 @@ class GameScene extends Phaser.Scene {
     this.load.image("arrow_yellow", "/assets/arrow_yellow.png");
     this.load.image("arrow_purple", "/assets/arrow_purple.png");
     this.load.audio("kick", "/assets/kick.mp3");
+    this.load.image('state1', 'assets/state1.png');
+    this.load.image('state2', 'assets/state2.png');
+    this.load.image('state3', 'assets/state3.png');
+    this.load.image('state4', 'assets/state4.png');
+    this.load.image('state5', 'assets/state5.png');
   }
 
   create() {
     this.scene.pause("scene-game");
 
     this.kick = this.sound.add("kick");
-    this.add.image(0, 0, "bg").setOrigin(0,  0);
+    this.add.image(0, 0, "bg").setOrigin(0, 0);
 
-    this.quadrants = [["arrow_blue", (sizes.width/4)*3, sizes.height/4], ["arrow_red", sizes.width/4, sizes.height/4], ["arrow_yellow", sizes.width/4, (sizes.height/4)*3], ["arrow_purple", (sizes.width/4)*3, (sizes.height/4)*3]];
+    this.quadrants = [["arrow_blue", (sizes.width / 4) * 3, sizes.height / 4], ["arrow_red", sizes.width / 4, sizes.height / 4], ["arrow_yellow", sizes.width / 4, (sizes.height / 4) * 3], ["arrow_purple", (sizes.width / 4) * 3, (sizes.height / 4) * 3]];
 
     this.timerEvent = this.time.addEvent({
       delay: beat, // Delay in milliseconds (1000 ms = 1 second)
@@ -48,17 +55,43 @@ class GameScene extends Phaser.Scene {
 
     let position = this.getRandomQuadrant();
     this.target = this.add.image(position[1], position[2], position[0]).setOrigin(0.5, 0.5);
+
+    // Create sprite and set its initial image
+    this.sprite = this.add.sprite(position[1], position[2], 'state1');
+
+    for (let i = 1; i <= 5; i++) {
+      this.anims.create({
+          key: `state${i}Animation`,
+          frames: [{key: `state${i}`}],
+          // this shit funkade ju inte
+          frameRate: 10, // Higher frame rate for smoother animation
+          ease: 'Linear', // Easing function for smooth acceleration
+          repeat: 0, // Don't repeat the animation
+      });
+  }
   }
 
   update() {
   }
 
   triggerEvent() {
+    if (time < 5) {
+      time++;
+    } else {
+      time = 0;
+    }
     this.kick.play();
     let newPosition = this.getRandomQuadrant();
     this.target.setTexture(newPosition[0]);
     this.target.setX(newPosition[1]);
     this.target.setY(newPosition[2]);
+
+    if (time == 0) {this.sprite.anims.play('state1Animation');}
+    else if (time == 1) {this.sprite.anims.play('state2Animation');}
+    else if (time == 2) {this.sprite.anims.play('state3Animation');}
+    else if (time == 3) {this.sprite.anims.play('state4Animation');}
+    else if (time == 4) {this.sprite.anims.play('state5Animation');}
+    
   }
 
   getRandomQuadrant() {
@@ -77,7 +110,7 @@ const config = {
   width: sizes.width,
   height: sizes.height,
   canvas: gamecanvas,
-  scene:[GameScene]
+  scene: [GameScene]
 };
 
 const game = new Phaser.Game(config);
